2849f65a09034c30c001107f26aeee2726c94d8c
xdy480 <dongyuxu11@gmail.com>
2020-07-08 13:55:47 +0800

    实现增删改页面增删改按钮切换页面

--- Assets/Scripts/CRUD.cs
+++ Assets/Scripts/CRUD.cs
@@ -5,15 +5,42 @@ using UnityEngine;
 //增删改脚本
 public class CRUD : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject CreateGameObject;                         //增加页面
+    public GameObject DeleteGameObject;                         //删除页面
+    public GameObject UpdateGameObject;                         //更新页面
+    private GameObject _obj;                                    //接收当前页面                
+
+    void Awake()
     {
-        
+        _obj = CreateGameObject;
     }
 
-    // Update is called once per frame
-    void Update()
+    //增加页面
+    public void Create()
     {
-        
+        Show(CreateGameObject);
+    }
+
+    //删除页面
+    public void Delete()
+    {
+        Show(DeleteGameObject);
+    }
+
+    //更新页面
+    public void UpdateWord()
+    {
+        Show(UpdateGameObject);
+    }
+
+    //页面的显示与隐藏
+    private void Show(GameObject obj)
+    {
+        if (_obj != obj)
+        {
+            _obj.SetActive(false);
+            _obj = obj;
+            _obj.SetActive(true);
+        }
     }
 }
